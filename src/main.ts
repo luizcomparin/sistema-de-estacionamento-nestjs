7e93bb1cba5710a2af283a9ebb5190de9152eca2
Luiz Comparin <luizcomparin18@gmail.com>
2023-10-17 11:52:50 -0300

    feat: swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.setGlobalPrefix('/api/v1')
 	app.useGlobalPipes(new ValidationPipe());
+
+	const config = new DocumentBuilder()
+		.setTitle('Sistema de Estacionamento')
+		.setDescription('Endpoints das APIs do sistema de gestão de estacionamento, usando Nest.js.')
+		.setVersion('1.0')
+		// .addTag('cats')
+		.build();
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('api', app, document);
+
 	await app.listen(3000);
 }
 bootstrap();
--- src/shared/response-object/common.response.ts
+++ src/shared/response-object/common.response.ts
@@ -1,6 +1,8 @@
 
 // EXEMPLO DO VITÃO DE COMO UTILIZAR INTERFACES GENERICAS PARA TIPAR O RESPONSE DAS APIS
 
+import { ApiProperty } from "@nestjs/swagger"
+
 export enum StatusEnum {
 	success = 'success',
 	failure = 'failure',
@@ -13,8 +15,11 @@ interface IResponse<T> {
 }
 
 export class CommonResponse<T> implements IResponse<T>{
+	@ApiProperty()
 	status: StatusEnum
+	@ApiProperty()
 	message: string
+	@ApiProperty()
 	result: T
 	constructor(status: StatusEnum, message: string, result: any) {
 		this.status = status,
